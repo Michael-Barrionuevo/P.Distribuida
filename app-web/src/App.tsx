68c2721cb6f1eb71223e5b0b0f2c4aae414147a4
Michael-Barrionuevo <mvbarrionuevos@uce.edu.ec>
2026-06-10 21:37:52 -0500

    Implementacion de docker

--- app-web/src/App.tsx
+++ app-web/src/App.tsx
@@ -3,73 +3,80 @@ import axios from "axios";
 import {useState} from "react";
 
 interface Author {
-  id: number;
-  name: string;
+    id: number;
+    name: string;
 }
 
 interface Book {
-  isbn: string;
-  price: number;
-  title: string;
-  author: Array<Author>;
+    isbn: string;
+    price: number;
+    title: string;
+    authors: Array<Author>;
 }
 
 function App() {
 
-  const [authors, setAuthors] = useState<Author[]>([]);
-  const [books, setBooks] = useState<Book[]>([]);
+    const [authors, setAuthors] = useState<Author[]>([]);
+    const [books, setBooks] = useState<Book[]>([]);
 
-  const handleListarAutores = () => {
-    axios.get<Author[]>("http://localhost/app-authors/authors")
-        .then(response => {
-          setAuthors(response.data);
+    const handleListarAutores = () => {
+        axios.get<Author[]>("http://localhost/app-authors/authors")
+            .then(response => {
+                setAuthors(response.data);
 
-        })
-        .catch(error => alert(error));
-  };
+            })
+            .catch(error => alert(error));
+    };
 
-  const handleListarBooks = () => {
-    axios.get<Book[]>("http://localhost/app-books/books")
-        .then(response => {
-          setBooks(response.data);
+    const handleListarBooks = () => {
+        axios.get<Book[]>("http://localhost/app-books/books")
+            .then(response => {
+                setBooks(response.data);
 
-        })
-        .catch(error => alert(error));
-  };
+            })
+            .catch(error => alert(error));
+    };
 
-  return (
-      <>
-        <section id="center">
-          <div>
-            <h2>Autores</h2>
-          </div>
-          <button onClick={handleListarAutores}>Consultar</button>
+    return (
+        <>
+            <section id="center">
+                <div>
+                    <h2>Autores</h2>
+                </div>
+                <button onClick={handleListarAutores}>Consultar</button>
+                <br/>
+                {
+                    authors.map(author =>
+                        <p key={author.id}>{author.id}-{author.name}</p>)
+                }
+            </section>
 
+            <br/>
 
-          {
-            authors.map(author =>
-                <p key={author.id}>{author.id}-{author.name}</p>)
-          }
-        </section>
+            <section id="center">
+                <div>
+                    <h2>Books</h2>
+                </div>
+                <button onClick={handleListarBooks}>Consultar</button>
+                <br/>
+                {
+                    books.map(book =>
+                        <ul key={book.isbn}>
+                            <li>{book.isbn}-{book.title}</li>
+                            {
+                                book.authors.map(author =>
+                                    <span key={author.id}>
+                                        {author.name}
+                                        <br/>
+                                    </span>
+                                )
+                            }
+                        </ul>)
+                }
+            </section>
 
-
-
-
-        <section id="center">
-          <div>
-            <h2>Books</h2>
-          </div>
-          <button onClick={handleListarBooks}>Consultar</button>
-
-
-          {
-            books.map(author =>
-                <p key={author.isbn}>{author.isbn}-{author.title}</p>)
-          }
-        </section>
-
-      </>
-  )
+        </>
+    )
 }
 
 export default App
